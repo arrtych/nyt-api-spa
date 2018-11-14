a2944ea1559c242cdad8b7c0e5848503cf4a8717
Artur Lipin <cookiess718@gmail.com>
2018-11-14 23:02:54 +0200

    requests with headers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 import Form from "./components/Form";
 import Article from "./components/Article";
 
@@ -10,30 +11,69 @@ class App extends React.Component {
     state = {
         title: undefined,
         fragment: undefined,
-        link: undefined
-    }
-    getData = async (e) => {
-        // e.preventDefault();
-        // const query = e.target.elements.query.value;
-        const api_call = await fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=6ba1190f0cd84209b031da93e7a7cb6e&q=design");
-        // await fetch(https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${API_KEY}&q=${QUERY});
-        const data = await api_call.json();
-        // var title = data.response.docs.headline.main;
-        var found = data.response.docs.find(function(e) {
-            return e;
-        });
-        console.log(found.headline.main);
-        this.setState({
-            title: found.headline.main,
-            fragment: found.snippet,
-            link: found.web_url
-        });
+        link: undefined,
+        query: ''
+    };
 
-    }
+    getData = async (e) => {
+        e.preventDefault();
+
+        const query = this.state.query;
+        // const api_call = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=3585775f387b0d0cba6c5b3dc41b8167&q=design`);
+        // const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${API_KEY}&q=${query}`;
+        // const url = `https://jsonplaceholder.typicode.com/users`;
+        const url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6ba1190f0cd84209b031da93e7a7cb6e`;
+
+        axios.get(url, {
+            headers: { 'Access-Control-Allow-Origin': '*' }
+        })
+            .then(data => {
+                console.log('DATA:', data);
+            })
+            .catch(err => console.log('ERR:', err));
+
+        return;
+        try {
+            // 'Access-Control-Allow-Origin':'*',
+            const api_call = await fetch(
+                `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${API_KEY}&q=${query}`,
+                {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                }
+            );
+            const data = await api_call.json();
+            // var title = data.response.docs.headline.main;
+            const found = data.response.docs.find(function(e) {
+                return e;
+            });
+            console.log(found.headline.main);
+            this.setState({
+                title: found.headline.main,
+                fragment: found.snippet,
+                link: found.web_url
+            });
+        } catch (err) {
+            console.log(err);
+        }
+
+
+
+    };
+
+    onQueryChanged = (e) => {
+        this.setState({ query: e.target.value })
+    };
+
     render() {
         return (
             <div>
-                <Form getData={this.getData()}/>
+                <Form
+                    getData={this.getData}
+                    query={this.state.query}
+                    onQueryChanged={this.onQueryChanged}
+                />
                 <Article
                     title={this.state.title}
                     fragment={this.state.fragment}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Form = props => (
-    <form onSubmit={props.getData}>
-        <input type="text" name="query" placeholder="Search..."/>
-        <input type="submit" value="Get Article" />
-    </form>
-);
+const Form = props => {
+    return (
+        <form onSubmit={props.getData}>
+            <input type="text" name="query" placeholder="Search..." value={props.query} onChange={props.onQueryChanged}/>
+            <input type="submit" value="Get Article" />
+        </form>
+    );
+};
 
 export default Form;
